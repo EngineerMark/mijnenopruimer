44c3a7a8fb754b6204403baf83b7bf49b51601ed
Mark Meijer <engineermarkpro@gmail.com>
2020-09-17 14:58:07 +0200

    GDD, charts and created project

---
+++ vulkaanruimer/Assets/Code/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject gridTile;
+
+    public Vector2 gridSize;
+
+    public void Start()
+    {
+        // Create a testgrid
+
+        for (int x = 0; x < gridSize.x; x++)
+        {
+            for (int y = 0; y < gridSize.y; y++)
+            {
+                Instantiate(gridTile, new Vector3(x, 0, y), Quaternion.identity);
+            }
+        }
+    }
+}
