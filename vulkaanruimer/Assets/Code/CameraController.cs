1f2c3696353a57666ea7b4b28cba9e6ccbff4128
Mark Meijer <engineermarkpro@gmail.com>
2020-09-24 11:43:13 +0200

    Camera rotating added, video settings save and apply

--- vulkaanruimer/Assets/Code/CameraController.cs
+++ vulkaanruimer/Assets/Code/CameraController.cs
@@ -7,14 +7,31 @@ public class CameraController : MonoBehaviour
 
     private Camera childCamera;
 
+    [Header("Move")]
+
+
+    [Header("Zoom")]
+
+
+    [Header("Rotation")]
+    public float rotSpeed = 3.5f;
+    private float rotX;
+    private float rotY;
+    public Vector2 rotXConstraint;
+
     void Start()
     {
         childCamera = transform.GetChild(0).GetComponent<Camera>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(2))
+        {
+            transform.Rotate(new Vector3(Input.GetAxis("Mouse Y") * rotSpeed, -Input.GetAxis("Mouse X") * rotSpeed, 0));
+            rotX = Mathf.Clamp(transform.rotation.eulerAngles.x, rotXConstraint.x, rotXConstraint.y);
+            rotY = transform.rotation.eulerAngles.y;
+            transform.rotation = Quaternion.Euler(rotX, rotY, 0);
+        }
     }
 }
--- vulkaanruimer/Assets/Code/UIManager.cs
+++ vulkaanruimer/Assets/Code/UIManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -9,6 +10,8 @@ public class UIManager : MonoBehaviour
     public Dropdown resolutionDropdown;
     public Toggle fullscreenToggle;
 
+    private bool currentFullscreen = false;
+
     void Start()
     {
         //Generate list of resolutions 
@@ -17,14 +20,48 @@ public class UIManager : MonoBehaviour
             resolutionDropdown.options.Add(new Dropdown.OptionData() { text = res.width + "x" + res.height });
         }
 
+        if(PlayerPrefs.HasKey("window_resX")){
+            int resX = PlayerPrefs.GetInt("window_resX");
+            int resY = PlayerPrefs.GetInt("window_resY");
+            Screen.SetResolution(resX, resY, false);
+            int dropdownValue = 0;
+            for(int i=0;i<resolutionDropdown.options.Count;i++){
+                var resItem = resolutionDropdown.options[i];
+                if (resItem.text==resX+"x"+resY){
+                    dropdownValue = i;
+                    break;
+                }
+            }
+            resolutionDropdown.value = dropdownValue;
+        }
+
+        resolutionDropdown.onValueChanged.AddListener(delegate
+        {
+            SwitchResolution(resolutionDropdown);
+        });
+
+        fullscreenToggle.isOn = PlayerPrefs.HasKey("window_Fullscreen")?PlayerPrefs.GetInt("window_Fullscreen")==1:true;
+        Screen.fullScreen = fullscreenToggle.isOn;
+        currentFullscreen = fullscreenToggle.isOn;
         fullscreenToggle.onValueChanged.AddListener(delegate
         {
             ToggleFullscreen(fullscreenToggle);
         });
     }
 
+    public void SwitchResolution(Dropdown dropdown){
+        string[] vals = dropdown.captionText.text.Split('x');
+        int resX = Convert.ToInt32(vals[0]);
+        int resY = Convert.ToInt32(vals[1]);
+        Screen.SetResolution(resX, resY, currentFullscreen);
+        PlayerPrefs.SetInt("window_resX", resX);
+        PlayerPrefs.SetInt("window_resY", resY);
+    }
+
     public void ToggleFullscreen(Toggle toggle){
         Screen.fullScreen = toggle.isOn;
+        currentFullscreen = toggle.isOn;
+        PlayerPrefs.SetInt("window_Fullscreen", toggle.isOn ? 1 : 0);
     }
 
     public void Quit()
